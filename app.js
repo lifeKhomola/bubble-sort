981eeabaa243a9610ade5c453a27f642cc6fca54
lifeKhomola <khomolavhutshilo5@gmail.com>
2021-12-06 09:46:36 +0200

    bubbel sort

---
+++ app.js
@@ -0,0 +1,18 @@
+function bubbleSort(arry)
+{
+    for(let i = 0; i < arr.length; i++)
+    {
+        for(let j = 0; j < ( arr.length - i -1 ); j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    console.log(arry);
+}
+var arr = [234, 43, 55, 63,  5, 6, 235, 547];
+console.log(bubbleSortz(arr));
